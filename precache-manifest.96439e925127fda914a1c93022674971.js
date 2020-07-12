db62ba4cc50b4b6ec66e26dfc1de9b5255e2b162
priyatank25 <priya10tank@gmail.com>
2020-07-12 22:23:56 +0530

    Updates

---
+++ precache-manifest.96439e925127fda914a1c93022674971.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69f29025663a8cf323d55198aa8888b",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "08c5efe97c65e8821bb2",
+    "url": "/myportfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "98ca2e1b236851709974",
+    "url": "/myportfolio/static/css/main.408bcaef.chunk.css"
+  },
+  {
+    "revision": "08c5efe97c65e8821bb2",
+    "url": "/myportfolio/static/js/2.5d990373.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myportfolio/static/js/2.5d990373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ca2e1b236851709974",
+    "url": "/myportfolio/static/js/main.138c02f0.chunk.js"
+  },
+  {
+    "revision": "2ae72037f9afc72e02ee",
+    "url": "/myportfolio/static/js/runtime-main.2295e036.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
